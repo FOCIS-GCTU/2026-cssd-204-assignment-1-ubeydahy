cd8ce4cf4d9c94135576692f82ff7a1a31f33c55
Ubeydah <ubeydahy@gmail.com>
2026-05-31 16:39:41 +0000

    Submit assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Ubeydah Yussuf
+# STUDENT ID:  2425403965
+# Email: ubeydahy@gmail.com
+# Grader: Augustus
+#
+# On my honor, Ubeydah Yussuf, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    #The A stands for my middle name(Ajoke) but I don't include it when writing my name,
+    # So I'm including it here, so that my initials can be 3.
 
+    print()
+    print("...UAY")
+    print()
+ 
+    print("...UU        UU...****...    AAAA    ...****...YY        YY...****")
+    print("...UU        UU...****...   AA  AA   ...****... YY      YY ...****")
+    print("...UU        UU...****...  AA    AA  ...****...  YY    YY  ...****")
+    print("...UU        UU...****... AA      AA ...****...   YY  YY   ...****")
+    print("...UU        UU...****...AAAAAAAAAAAA...****...    YYYY    ...****")
+    print("...UU        UU...****...AA        AA...****...     YY     ...****")
+    print("...UU        UU...****...AA        AA...****...     YY     ...****")
+    print("...UU        UU...****...AA        AA...****...     YY     ...****")
+    print("... UU      UU ...****...AA        AA...****...     YY     ...****")
+    print("...  UUUUUUUU  ...****...AA        AA...****...     YY     ...****")
 
+    print()
 
 main()
